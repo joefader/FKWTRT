f60109f773de56a01977981d102a3e9b03bc4832
Joseph Fader <joefader@gmail.com>
2018-12-17 16:23:21 -0500

    filter by date for each row works, still working on adding in distance

--- Scripts/filter_by_spacetime.R
+++ Scripts/filter_by_spacetime.R
@@ -28,18 +28,42 @@ testday <- mdy(sets_deep_all$HAUL_BEGIN_DATE[1000])
 
 testday - days(3)
 
-filter_func <- function(df, haulday){
-    temp <- filter(df, mdy(HAUL_BEGIN_DATE) >= testday - days(3) & mdy(HAUL_BEGIN_DATE) <= testday + days(3))
-    View(temp)
-    mean(temp$CPUE)
+# create column in df to accept averaged values
+sets_deep_all <- sets_deep_all %>% mutate(avg_cpue = 0)
+
+# make test dataset to play with
+sets_deep_test <- sets_deep_all %>% select(-c(SOAK:NONTARGET)) %>% filter(YEAR == 2010 & MONTH == 5)
+
+
+# filtering function, finds all sets within specified space/time and puts in temp file
+filter_func <- function(x){
+    sets_deep_test %>%
+      filter(mdy(HAUL_BEGIN_DATE) >= mdy(x["HAUL_BEGIN_DATE"]) - days(1) &
+                     mdy(HAUL_BEGIN_DATE) <= mdy(x["HAUL_BEGIN_DATE"]) + days(1)) %>%
+      summarise(mean(CPUE, na.rm=T)) %>% as_vector()
   }
 
-filter_func(sets_deep_all, testday)
+sets_deep_test[, "avg_cpue"] <- apply(sets_deep_test, 1, filter_func)
 
-sets_deep_all %>% 
-  mutate(HAUL_BEGIN_DATE)
-mutate(DP_LAG_NUM = ifelse(SET_NUM == (lag(SET_NUM) + 1), DP_LAG_NUM, NA))
 
+
+#### replicate for distance
+filter_dist <- function(x){
+  sets_deep_test %>%
+    filter(mdy(HAUL_BEGIN_DATE) >= mdy(x["HAUL_BEGIN_DATE"]) - days(1) &
+             mdy(HAUL_BEGIN_DATE) <= mdy(x["HAUL_BEGIN_DATE"]) + days(1)) %>%
+    rdist.earth.vec(matrix(c(HAUL_BEGIN_LON, HAUL_BEGIN_LAT), ncol=2),
+                    matrix(c(x["HAUL_BEGIN_LON"], x['HAUL_BEGIN_LAT']), ncol=2), 
+                           miles = F, R = 6371) 
+    #summarise(mean(CPUE, na.rm=T)) %>% as_vector()
+}
+
+sets_deep_test[, "avg_cpue"] <- apply(sets_deep_test, 1, filter_dist)
+
+rdist.earth
+
+rdist.earth
+#
 sets_alldata <- sets_alldata %>%
   mutate(LAG_DIST = rdist.earth.vec(matrix(c(SET_BEGIN_LON, SET_BEGIN_LAT), ncol=2), matrix(c(lag(HAUL_END_LON), lag(HAUL_END_LAT)), ncol=2), miles = F, R = 6371))
 sets_alldata[which(sets_alldata$SET_NUM == 1), "LAG_DIST"] <- NA
